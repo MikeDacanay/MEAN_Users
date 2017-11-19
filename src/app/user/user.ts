b00638daf194452e30956fa26629fd10486ae289
Mike Dacanay <migd64@gmail.com>
2017-11-19 15:50:13 -0800

    8:10

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,2 +1,8 @@
 export class User {
+	constructor(
+		public _id: number = Math.floor(math.random()*100),
+		public first_name: string = "",
+		public last_name: string = "",
+		public email_name: string = "",
+	){}
 }
